9b7de6193a54a39ed54c3848f497c8011c7f2968
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-08-10 18:34:14 -0500

    remove vanilla ores and add modded gem ores, vanilla ores wip

--- minecraft/kubejs/startup_scripts/emendatusJS/emenOredefinitions.js
+++ minecraft/kubejs/startup_scripts/emendatusJS/emenOredefinitions.js
@@ -27,6 +27,117 @@ global.dimensionsOreData = {
     }
 }
 
+// coal copper diamond emerald gold iron lapis quartz redstone
+// vanilla
+// global.emendatus_mats['coal'].oreData = {
+//     dropType: 'gem',
+//     dropCountRange: [1, 1],
+//     airDiscardChance: 0,
+//     dimensions: {
+//         "minecraft:overworld": {
+//             range :[-60, 256],
+//             count: 20,
+//             size: 25
+//         }
+//     }
+// }
+// global.emendatus_mats['copper'].oreData = {
+//     dropType: 'raw_ore',
+//     dropCountRange: [1, 1],
+//     airDiscardChance: 0,
+//     dimensions: {
+//         "minecraft:overworld": {
+//             range :[-60, 256],
+//             count: 20,
+//             size: 25
+//         }
+//     }
+// }
+// global.emendatus_mats['diamond'].oreData = {
+//     dropType: 'gem',
+//     dropCountRange: [1, 1],
+//     airDiscardChance: 0,
+//     dimensions: {
+//         "minecraft:overworld": {
+//             range :[-60, 256],
+//             count: 20,
+//             size: 25
+//         }
+//     }
+// }
+// global.emendatus_mats['emerald'].oreData = {
+//     dropType: 'gem',
+//     dropCountRange: [1, 1],
+//     airDiscardChance: 0,
+//     dimensions: {
+//         "minecraft:overworld": {
+//             range :[-60, 256],
+//             count: 20,
+//             size: 25
+//         }
+//     }
+// }
+// global.emendatus_mats['gold'].oreData = {
+//     dropType: 'raw_ore',
+//     dropCountRange: [1, 1],
+//     airDiscardChance: 0,
+//     dimensions: {
+//         "minecraft:overworld": {
+//             range :[-60, 256],
+//             count: 20,
+//             size: 25
+//         }
+//     }
+// }
+// global.emendatus_mats['iron'].oreData = {
+//     dropType: 'raw_ore',
+//     dropCountRange: [1, 1],
+//     airDiscardChance: 0,
+//     dimensions: {
+//         "minecraft:overworld": {
+//             range :[-60, 256],
+//             count: 20,
+//             size: 25
+//         }
+//     }
+// }
+// global.emendatus_mats['lapis'].oreData = {
+//     dropType: 'gem',
+//     dropCountRange: [1, 1],
+//     airDiscardChance: 0,
+//     dimensions: {
+//         "minecraft:overworld": {
+//             range :[-60, 256],
+//             count: 20,
+//             size: 25
+//         }
+//     }
+// }
+// global.emendatus_mats['quartz'].oreData = {
+//     dropType: 'gem',
+//     dropCountRange: [1, 1],
+//     airDiscardChance: 0,
+//     dimensions: {
+//         "minecraft:the_nether": {
+//             range :[-60, 256],
+//             count: 20,
+//             size: 25
+//         }
+//     }
+// }
+// global.emendatus_mats['redstone'].oreData = {
+//     dropType: 'dust',
+//     dropCountRange: [1, 1],
+//     airDiscardChance: 0,
+//     dimensions: {
+//         "minecraft:overworld": {
+//             range :[-60, 256],
+//             count: 20,
+//             size: 25
+//         }
+//     }
+// }
+
 // aluminium lead nickel platinum silver tin uranium zinc cobalt osmium iesnium
 // metals
 global.emendatus_mats['aluminum'].oreData = {
@@ -149,6 +260,126 @@ global.emendatus_mats['osmium'].oreData = {
         }
     }
 }
+
+// apatite cinnabar dimensional potassium_nitrate sapphire sulfur arcane certus_quartz fluorite ruby
 // gems
-
-
+global.emendatus_mats['apatite'].oreData = {
+    dropType: 'gem',
+    dropCountRange: [1, 1],
+    airDiscardChance: 0,
+    dimensions: {
+        "minecraft:overworld": {
+            range :[-60, 256],
+            count: 20,
+            size: 25
+        }
+    }
+}
+global.emendatus_mats['cinnabar'].oreData = {
+    dropType: 'gem',
+    dropCountRange: [1, 1],
+    airDiscardChance: 0,
+    dimensions: {
+        "minecraft:overworld": {
+            range :[-60, 256],
+            count: 20,
+            size: 25
+        }
+    }
+}
+global.emendatus_mats['dimensional'].oreData = {
+    dropType: 'gem',
+    dropCountRange: [1, 1],
+    airDiscardChance: 0,
+    dimensions: {
+        "minecraft:overworld": {
+            range :[-60, 256],
+            count: 20,
+            size: 25
+        }
+    }
+}
+global.emendatus_mats['potassium_nitrate'].oreData = {
+    dropType: 'gem',
+    dropCountRange: [1, 1],
+    airDiscardChance: 0,
+    dimensions: {
+        "minecraft:overworld": {
+            range :[-60, 256],
+            count: 20,
+            size: 25
+        }
+    }
+}
+global.emendatus_mats['sapphire'].oreData = {
+    dropType: 'gem',
+    dropCountRange: [1, 1],
+    airDiscardChance: 0,
+    dimensions: {
+        "minecraft:overworld": {
+            range :[-60, 256],
+            count: 20,
+            size: 25
+        }
+    }
+}
+global.emendatus_mats['sulfur'].oreData = {
+    dropType: 'gem',
+    dropCountRange: [1, 1],
+    airDiscardChance: 0,
+    dimensions: {
+        "minecraft:overworld": {
+            range :[-60, 256],
+            count: 20,
+            size: 25
+        }
+    }
+}
+global.emendatus_mats['arcane'].oreData = {
+    dropType: 'gem',
+    dropCountRange: [1, 1],
+    airDiscardChance: 0,
+    dimensions: {
+        "minecraft:overworld": {
+            range :[-60, 256],
+            count: 20,
+            size: 25
+        }
+    }
+}
+global.emendatus_mats['certus_quartz'].oreData = {
+    dropType: 'gem',
+    dropCountRange: [1, 1],
+    airDiscardChance: 0,
+    dimensions: {
+        "minecraft:overworld": {
+            range :[-60, 256],
+            count: 20,
+            size: 25
+        }
+    }
+}
+global.emendatus_mats['fluorite'].oreData = {
+    dropType: 'gem',
+    dropCountRange: [1, 1],
+    airDiscardChance: 0,
+    dimensions: {
+        "minecraft:overworld": {
+            range :[-60, 256],
+            count: 20,
+            size: 25
+        }
+    }
+}
+global.emendatus_mats['ruby'].oreData = {
+    dropType: 'gem',
+    dropCountRange: [1, 1],
+    airDiscardChance: 0,
+    dimensions: {
+        "minecraft:overworld": {
+            range :[-60, 256],
+            count: 20,
+            size: 25
+        }
+    }
+}
--- minecraft/kubejs/startup_scripts/worldgen/oreRemoval.js
+++ minecraft/kubejs/startup_scripts/worldgen/oreRemoval.js
@@ -38,10 +38,27 @@ WorldgenEvents.remove(event => {
     event.printFeatures('underground_ores')
     // event.removeFeatureById('lakes', 'pneumaticcraft:oil_lake_surface')
     // event.removeFeatureById('lakes', 'pneumaticcraft:oil_lake_underground')
-    
+    // coal copper diamond emerald gold iron lapis quartz redstone
     event.removeOres(props => {
         props.blocks = [
-            // "minecraft:coal_ore",
+            "minecraft:coal_ore",
+            "minecraft:deepslate_coal_ore",
+            "minecraft:copper_ore",
+            "minecraft:deepslate_copper_ore",
+            "minecraft:diamond_ore",
+            "minecraft:deepslate_diamond_ore",
+            "minecraft:emerald_ore",
+            "minecraft:deepslate_emerald_ore",
+            "minecraft:gold_ore",
+            "minecraft:deepslate_gold_ore",
+            "minecraft:nether_gold_ore",
+            "minecraft:iron_ore",
+            "minecraft:deepslate_iron_ore",
+            "minecraft:lapis_ore",
+            "minecraft:deepslate_lapis_ore",
+            "minecraft:nether_quartz_ore",
+            "minecraft:redstone_ore",
+            "minecraft:deepslate_redstone_ore",
 
             "thermal:apatite_ore",
             "thermal:deepslate_apatite_ore",
