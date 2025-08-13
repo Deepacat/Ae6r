0e6b3985e923368e897535771ca1e4ce06917bff
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-08-13 13:00:53 -0500

    fix tinkers gem fluid vals

--- minecraft/kubejs/server_scripts/emendatusJS/emenUniRecipes.js
+++ minecraft/kubejs/server_scripts/emendatusJS/emenUniRecipes.js
@@ -109,11 +109,13 @@ function smeltingRecipes(e, matObj, matName, inputFlagType) {
 
     if (validateFlag(inputFlagType, matObj)) {
         let itemTag = getTagReplace(inputFlagType, matName)
+        let fluidAmt = 90
+        if (inputFlagType == 'gem') { fluidAmt = 100 }
         // scrap melting
         if (Fluid.exists(`tconstruct:molten_${matName}`)) {
-            e.recipes.thermal.crucible(Fluid.of(`tconstruct:molten_${matName}`, 90), `#${itemTag}`, 0, 2000)
+            e.recipes.thermal.crucible(Fluid.of(`tconstruct:molten_${matName}`, fluidAmt), `#${itemTag}`, 0, 2000)
                 .id(`${prefix}thermal/crucible/${matName}_${inputFlagType}`)
-            embersMelting(e, Fluid.of(`tconstruct:molten_${matName}`, 90), `forge:${inputFlagType}s/${matName}`)
+            embersMelting(e, Fluid.of(`tconstruct:molten_${matName}`, fluidAmt), `forge:${inputFlagType}s/${matName}`)
         }
     }
 }
@@ -122,26 +124,35 @@ function plateRecipes(e, matObj, matName, inputFlagType) {
     if (validateFlag('plate', matObj)) {
         let input = '#' + getTagReplace(inputFlagType, matName)
         let output = prefix + getFlagReplace('plate', matName)
+
         // fluid recipes
         if (Fluid.exists(`tconstruct:molten_${matName}`)) {
-            e.recipes.thermal.chiller(output, ['tconstruct:plate_cast', Fluid.of(`tconstruct:molten_${matName}`, 90)])
+            let fluidAmt = 90
+            if (inputFlagType == 'gem') { fluidAmt = 100 }
+            e.recipes.thermal.chiller(output, ['tconstruct:plate_cast', Fluid.of(`tconstruct:molten_${matName}`, fluidAmt)])
                 .id(`${prefix}thermal/chiller/${matName}_plate`)
-            e.recipes.thermal.crucible(Fluid.of(`tconstruct:molten_${matName}`, 90), '#' + getTagReplace('plate', matName), 0, 2000)
+
+            e.recipes.thermal.crucible(Fluid.of(`tconstruct:molten_${matName}`, fluidAmt), '#' + getTagReplace('plate', matName), 0, 2000)
                 .id(`${prefix}thermal/crucible/${matName}_plate`)
-            embersMelting(e, Fluid.of(`tconstruct:molten_${matName}`, 90), `forge:plates/${matName}`)
+
+            embersMelting(e, Fluid.of(`tconstruct:molten_${matName}`, fluidAmt), `forge:plates/${matName}`)
         }
-        // item recipes
+
         e.recipes.create.pressing(Item.of(output), input)
             .id(`${prefix}create/pressing/${matName}_plate`)
+
         e.recipes.thermal.press(output, input)
             .id(`${prefix}thermal/press/${matName}_plate`)
+
         e.recipes.immersiveengineering.metal_press(`4x ${output}`, `4x ${input}`, 'immersiveengineering:mold_plate')
             .id(`${prefix}immersiveengineering/metalpress/${matName}_plate`)
+
         e.custom({
             type: "createdieselgenerators:hammering",
             ingredients: [{ tag: getTagReplace(inputFlagType, matName) }],
             results: [{ item: output }]
         }).id(`${prefix}createdieselgenerators/hammering/${matName}_plate`)
+
         e.shaped(output, [
             'H  ',
             'I  ',
@@ -157,27 +168,37 @@ function rodRecipes(e, matObj, matName, inputFlagType) {
     if (validateFlag('rod', matObj)) {
         let input = '#' + getTagReplace(inputFlagType, matName)
         let output = prefix + getFlagReplace('rod', matName)
+        // recipes using fluid
         if (Fluid.exists(`tconstruct:molten_${matName}`)) {
+            let fluidAmt = 45
+            if (inputFlagType == 'gem') { fluidAmt = 50 }
             e.recipes.thermal.chiller(output, ['tconstruct:rod_cast', Fluid.of(`tconstruct:molten_${matName}`, 45)])
                 .id(`${prefix}thermal/chiller/${matName}_rod`)
+
             e.recipes.thermal.crucible(Fluid.of(`tconstruct:molten_${matName}`, 45), '#' + getTagReplace('rod', matName), 0, 1000)
                 .id(`${prefix}thermal/crucible/${matName}_rod`)
-            embersMelting(e, Fluid.of(`tconstruct:molten_${matName}`, 45), `forge:rods/${matName}`)
+
+            embersMelting(e, Fluid.of(`tconstruct:molten_${matName}`, fluidAmt), `forge:rods/${matName}`)
         }
+
         e.recipes.thermal.press(`2x ${output}`, [input, 'immersiveengineering:mold_rod'])
             .id(`${prefix}thermal/press/${matName}_rod`)
+
         e.recipes.immersiveengineering.metal_press(`8x ${output}`, `4x ${input}`, 'immersiveengineering:mold_rod')
             .id(`${prefix}immersiveengineering/metalpress/${matName}_rod`)
+
         e.custom({
             type: "createaddition:rolling",
             input: { tag: getTagReplace(inputFlagType, matName) },
             result: { item: output, count: 2 }
         }).id(`${prefix}createaddition/rolling/${matName}_rod`)
+
         e.custom({
             type: "createdieselgenerators:wire_cutting",
             ingredients: [{ tag: getTagReplace(inputFlagType, matName) }],
             results: [{ item: output, count: 2 }]
         }).id(`${prefix}createdieselgenerators/wire_cutting/${matName}_rod`)
+
         e.shaped(`2x ${output}`, [
             'IH ',
             'I  '
@@ -192,18 +213,26 @@ function gearRecipes(e, matObj, matName, inputFlagType) {
     if (validateFlag('gear', matObj)) {
         let input = '#' + getTagReplace(inputFlagType, matName)
         let output = prefix + getFlagReplace('gear', matName)
+        // recipes using fluid
         if (Fluid.exists(`tconstruct:molten_${matName}`)) {
-            e.recipes.thermal.chiller(output, ['tconstruct:gear_cast', Fluid.of(`tconstruct:molten_${matName}`, 360)])
+            let fluidAmt = 360
+            if (inputFlagType == 'gem') { fluidAmt = 400 }
+            e.recipes.thermal.chiller(output, ['tconstruct:gear_cast', Fluid.of(`tconstruct:molten_${matName}`, fluidAmt)])
                 .id(`${prefix}thermal/chiller/${matName}_gear`)
-            embersStamping(e, output, { fluid: `tconstruct:molten_${matName}`, amount: 360 }, 'embers:gear_stamp')
-            e.recipes.thermal.crucible(Fluid.of(`tconstruct:molten_${matName}`, 360), '#' + getTagReplace('gear', matName), 0, 8000)
+
+            embersStamping(e, output, { fluid: `tconstruct:molten_${matName}`, amount: fluidAmt }, 'embers:gear_stamp')
+
+            e.recipes.thermal.crucible(Fluid.of(`tconstruct:molten_${matName}`, fluidAmt), '#' + getTagReplace('gear', matName), 0, 8000)
                 .id(`${prefix}thermal/crucible/${matName}_gear`)
-            embersMelting(e, Fluid.of(`tconstruct:molten_${matName}`, 360), `forge:gears/${matName}`)
+
+            embersMelting(e, Fluid.of(`tconstruct:molten_${matName}`, fluidAmt), `forge:gears/${matName}`)
         }
         e.recipes.thermal.press(output, [`4x ${input}`, 'immersiveengineering:mold_gear'])
             .id(`${prefix}thermal/press/${matName}_gear`)
+
         e.recipes.immersiveengineering.metal_press(`4x ${output}`, `16x ${input}`, 'immersiveengineering:mold_gear')
             .id(`${prefix}immersiveengineering/metalpress/${matName}_gear`)
+
         e.shaped(output, [
             'NIN',
             "IPI",
