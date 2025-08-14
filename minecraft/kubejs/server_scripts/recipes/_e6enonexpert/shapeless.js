20fee9dd3bdbbb44f02fdb4d0836d810149ff92b
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-08-14 04:20:32 -0500

    base shapeless

---
+++ minecraft/kubejs/server_scripts/recipes/_e6enonexpert/shapeless.js
@@ -0,0 +1,335 @@
+ServerEvents.recipes(e => {
+    e.shapeless('botania:enchanted_soil',
+        ['minecraft:grass_block', 'botania:overgrowth_seed']
+    ).id('kubejs:shapeless/enchanted_soil')
+
+    e.shapeless('minecraft:sticky_piston',
+        ['minecraft:piston', '#forge:slimeballs']
+    ).id('kubejs:shapeless/sticky_piston')
+
+    e.shapeless('minecraft:flint',
+        ['#forge:gravel', '#forge:gravel', '#forge:gravel']
+    ).id('kubejs:shapeless/flint')
+
+    e.shapeless('minecraft:chest',
+        ['#forge:chests/wooden']
+    ).id('kubejs:shapeless/chest')
+
+    e.shapeless(Item.of('powah:uraninite', 9),
+        ['#forge:storage_blocks/uraninite']
+    ).id('kubejs:shapeless/uraninite')
+
+    e.shapeless(Item.of('farmersdelight:milk_bottle', 4),
+        ['minecraft:milk_bucket', 'minecraft:glass_bottle', 'minecraft:glass_bottle', 'minecraft:glass_bottle', 'minecraft:glass_bottle']
+    ).id('kubejs:shapeless/milk_bottle')
+
+    e.shapeless('minecraft:milk_bucket',
+        ['minecraft:bucket', 'farmersdelight:milk_bottle', 'farmersdelight:milk_bottle', 'farmersdelight:milk_bottle', 'farmersdelight:milk_bottle']
+    ).id('kubejs:shapeless/milk_bucket')
+
+    e.shapeless('minecraft:wheat_seeds',
+        ['minecraft:wheat']
+    ).id('kubejs:shapeless/wheat_seeds')
+
+    e.shapeless('minecraft:crafting_table',
+        ['#forge:workbenches']
+    ).id('kubejs:shapeless/crafting_table')
+
+    e.shapeless('ars_nouveau:source_gem',
+        ['emendatus:arcane_gem']
+    ).id('kubejs:shapeless/source_gem')
+
+    e.shapeless('emendatus:arcane_gem',
+        ['ars_nouveau:source_gem']
+    ).id('kubejs:shapeless/arcane_gem')
+
+    e.shapeless('botania:fertilizer',
+        ['#forge:fertilizer', '#forge:dyes', '#forge:dyes', '#forge:dyes', '#forge:dyes']
+    ).id('kubejs:shapeless/fertilizer')
+
+    e.shapeless('emendatus:dimensional_gem',
+        ['rftoolsbase:dimensionalshard']
+    ).id('kubejs:shapeless/dimensional_gem')
+
+    e.shapeless('rftoolsbase:dimensionalshard',
+        ['emendatus:dimensional_gem']
+    ).id('kubejs:shapeless/dimensionalshard')
+
+    // e.shapeless(Item.of('#forge:shards/ender'),
+    //     ['#forge:ores/ender', '#forge:tools/crafting_hammer']
+    // ).id('kubejs:shapeless/shards/ender')
+
+    // e.shapeless(Item.of('#forge:shards/amber'),
+    //     ['#forge:ores/amber', '#forge:tools/crafting_hammer']
+    // ).id('kubejs:shapeless/shards/amber')
+
+    // e.shapeless(Item.of('#forge:shards/ender'),
+    //     ['#forge:ores/ender', 'thermal:earth_charge']
+    // ).id('kubejs:shapeless/shards/ender')
+
+    // e.shapeless(Item.of('#forge:shards/amber'),
+    //     ['#forge:ores/amber', 'thermal:earth_charge']
+    // ).id('kubejs:shapeless/shards/amber')
+
+    e.shapeless('emendatus:fluorite_dust',
+        ['thermal:earth_charge', '#forge:ores/fluorite']
+    ).id('kubejs:shapeless/fluorite_dust')
+
+    e.shapeless('emendatus:dimensional_dust',
+        ['thermal:earth_charge', '#forge:ores/dimensional']
+    ).id('kubejs:shapeless/dimensional_dust')
+
+    e.shapeless('emendatus:potassium_nitrate_dust',
+        ['thermal:earth_charge', '#forge:ores/potassium_nitrate']
+    ).id('kubejs:shapeless/potassium_nitrate_dust')
+
+    e.shapeless('minecraft:writable_book',
+        ['minecraft:book', '#forge:dyes/black', '#forge:feathers']
+    ).id('kubejs:shapeless/writable_book')
+
+    e.shapeless(Item.of('minecraft:honeycomb', 9),
+        ['minecraft:honeycomb_block']
+    ).id('kubejs:shapeless/honeycomb')
+
+    e.shapeless(Item.of('minecraft:paper', 3),
+        ['minecraft:sugar_cane', 'minecraft:sugar_cane', 'minecraft:sugar_cane']
+    ).id('kubejs:shapeless/paper')
+
+    e.shapeless('supplementaries:flax_seeds',
+        ['supplementaries:flax']
+    ).id('kubejs:shapeless/flax_seeds')
+
+    e.shapeless('kubejs:quintuple_alfsteel_ingot',
+        ['#forge:ingots/alfsteel', '#forge:ingots/alfsteel', '#forge:ingots/alfsteel', '#forge:ingots/alfsteel', '#forge:ingots/alfsteel', '#forge:dusts/mana']
+    ).id('kubejs:shapeless/quintuple_alfsteel_ingot')
+
+    e.shapeless(Item.of('minecraft:terracotta'),
+        ['#enigmatica:washables/terracotta', 'minecraft:water_bucket']
+    ).id('kubejs:shapeless/terracotta')
+
+    e.shapeless('mythicbotany:raindeletia_floating',
+        ['kubejs:disabled_recipe_indicator']
+    ).id('kubejs:shapeless/raindeletia_floating')
+
+    e.shapeless('mythicbotany:wither_aconite_floating',
+        ['kubejs:disabled_recipe_indicator']
+    ).id('kubejs:shapeless/wither_aconite_floating')
+
+    e.shapeless(Item.of('eidolon:pewter_blend', 2),
+        ['#forge:dusts/lead', '#forge:dusts/iron']
+    ).id('kubejs:shapeless/pewter_blend')
+
+    e.shapeless(Item.of('minecraft:string', 3),
+        ['#forge:crops/kenaf', '#forge:crops/kenaf', '#forge:crops/kenaf']
+    ).id('kubejs:shapeless/string_kenaf')
+
+    e.shapeless(Item.of('minecraft:string', 3),
+        ['#forge:crops/flax', '#forge:crops/flax', '#forge:crops/flax']
+    ).id('kubejs:shapeless/string_flax')
+
+    e.shapeless(Item.of('minecraft:green_dye', 2),
+        ['#forge:dyes/blue', '#forge:dyes/yellow']
+    ).id('kubejs:shapeless/green_dye')
+
+    e.shapeless(Item.of('minecraft:brown_dye', 2),
+        ['#forge:dyes/red', '#forge:dyes/green']
+    ).id('kubejs:shapeless/brown_dye')
+
+    e.shapeless(Item.of('ars_nouveau:source_berry_roll', 3),
+        ['farmersdelight:wheat_dough', 'farmersdelight:wheat_dough', 'farmersdelight:wheat_dough', '#forge:fruits/mana_berry']
+    ).id('kubejs:shapeless/source_berry_roll')
+
+    e.shapeless('minecraft:charcoal',
+        ['#chisel:charcoal']
+    ).id('kubejs:shapeless/charcoal')
+
+    e.remove({ output: 'akashictome:tome' })
+    e.shapeless(Item.of('akashictome:tome', akashicTomeData),
+        ['minecraft:book', '#forge:bookshelves']
+    ).id('kubejs:shapeless/tome')
+})
+
+const akashicTomeData = 
+`{
+    "akashictome:data": {
+        "advancedperipherals": {
+            "Count": 1,
+            "id": "patchouli:guide_book",
+            "tag": {
+                "patchouli:book": "advancedperipherals:manual"
+            }
+        },
+        "alexsmobs": {
+            "Count": 1,
+            "id": "alexsmobs:animal_dictionary"
+        },
+        "apotheosis": {
+            "Count": 1,
+            "id": "patchouli:guide_book",
+            "tag": {
+                "patchouli:book": "apotheosis:apoth_chronicle"
+            }
+        },
+        "ars_nouveau": {
+            "Count": 1,
+            "id": "ars_nouveau:worn_notebook"
+        },
+        "bloodmagic": {
+            "Count": 1,
+            "id": "patchouli:guide_book",
+            "tag": {
+                "patchouli:book": "bloodmagic:guide"
+            }
+        },
+        "botania": {
+            "Count": 1,
+            "id": "botania:lexicon",
+            "tag": {
+                "botania:elven_unlock": 1
+            }
+        },
+        "buildinggadgets2": {
+            "Count": 1,
+            "id": "patchouli:guide_book",
+            "tag": {
+                "patchouli:book": "buildinggadgets2:buildinggadgets2book"
+            }
+        },
+        "cookingforblockheads": {
+            "Count": 1,
+            "id": "cookingforblockheads:crafting_book"
+        },
+        "eidolon": {
+            "Count": 1,
+            "id": "eidolon:codex"
+        },
+        "embers": {
+            "Count": 1,
+            "id": "embers:ancient_codex"
+        },
+        "ftbquests": {
+            "Count": 1,
+            "id": "ftbquests:book"
+        },
+        "immersiveengineering": {
+            "Count": 1,
+            "id": "immersiveengineering:manual"
+        },
+        "industrialforegoing": {
+            "Count": 1,
+            "id": "patchouli:guide_book",
+            "tag": {
+                "patchouli:book": "industrialforegoing:industrial_foregoing"
+            }
+        },
+        "integratedtunnels": {
+            "Count": 1,
+            "id": "integrateddynamics:on_the_dynamics_of_integration"
+        },
+        "laserio": {
+            "Count": 1,
+            "id": "patchouli:guide_book",
+            "tag": {
+                "patchouli:book": "laserio:laseriobook"
+            }
+        },
+        "littlelogistics": {
+            "Count": 1,
+            "id": "patchouli:guide_book",
+            "tag": {
+                "patchouli:book": "littlelogistics:guide"
+            }
+        },
+        "modularrouters": {
+            "Count": 1,
+            "id": "patchouli:guide_book",
+            "tag": {
+                "patchouli:book": "modularrouters:book"
+            }
+        },
+        "naturesaura": {
+            "Count": 1,
+            "id": "patchouli:guide_book",
+            "tag": {
+                "patchouli:book": "naturesaura:book"
+            }
+        },
+        "occultism": {
+            "Count": 1,
+            "id": "occultism:dictionary_of_spirits",
+            "tag": {
+                "modonomicon:book_id": "occultism:dictionary_of_spirits"
+            }
+        },
+        "pneumaticcraft": {
+            "Count": 1,
+            "id": "patchouli:guide_book",
+            "tag": {
+                "patchouli:book": "pneumaticcraft:book"
+            }
+        },
+        "powah": {
+            "Count": 1,
+            "id": "powah:book"
+        },
+        "rftoolsbase": {
+            "Count": 1,
+            "id": "rftoolsbase:manual"
+        },
+        "tconstruct": {
+            "Count": 1,
+            "id": "tconstruct:materials_and_you"
+        },
+        "tconstruct_0": {
+            "Count": 1,
+            "id": "tconstruct:puny_smelting",
+            "tag": {
+                "akashictome:definedMod": "tconstruct_0"
+            }
+        },
+        "tconstruct_1": {
+            "Count": 1,
+            "id": "tconstruct:mighty_smelting",
+            "tag": {
+                "akashictome:definedMod": "tconstruct_1"
+            }
+        },
+        "tconstruct_2": {
+            "Count": 1,
+            "id": "tconstruct:tinkers_gadgetry",
+            "tag": {
+                "akashictome:definedMod": "tconstruct_2"
+            }
+        },
+        "tconstruct_3": {
+            "Count": 1,
+            "id": "tconstruct:fantastic_foundry",
+            "tag": {
+                "akashictome:definedMod": "tconstruct_3"
+            }
+        },
+        "tconstruct_4": {
+            "Count": 1,
+            "id": "tconstruct:encyclopedia",
+            "tag": {
+                "akashictome:definedMod": "tconstruct_4",
+                "mantle": {
+                    "book": {
+                        "current_page": ""
+                    }
+                }
+            }
+        },
+        "thermal": {
+            "Count": 1,
+            "id": "patchouli:guide_book",
+            "tag": {
+                "patchouli:book": "thermal:guidebook"
+            }
+        },
+        "wizards_reborn": {
+            "Count": 1,
+            "id": "wizards_reborn:arcanemicon"
+        },
+    }
+}`
--- tools/e6recipefixer.js
+++ tools/e6recipefixer.js
@@ -1,68 +1,68 @@
 const getString = (input) => {
-	if (input == null) return
-	if (typeof input === "number") return input
-	if (input.item) return input
-	if (Array.isArray(input)) {
-		return `[${input.map(getString).join(", ")}]`
-	}
-	if (typeof input === "string") return `'${input}'`
-	if (typeof input === "object") {
-		return `{ ${Object.entries(input).map(([key, value]) => `${key}: ${getString(value)}`).join(", ")} }`
-	}
+    if (input == null) return
+    if (typeof input === "number") return input
+    if (input.item) return input
+    if (Array.isArray(input)) {
+        return `[${input.map(getString).join(", ")}]`
+    }
+    if (typeof input === "string") return `'${input}'`
+    if (typeof input === "object") {
+        return `{ ${Object.entries(input).map(([key, value]) => `${key}: ${getString(value)}`).join(", ")} }`
+    }
 }
 
 function shouldRemove(recipe) { return (recipe.id.includes('kubejs:') ? '' : 'e.remove({ id: "' + recipe.id + '" })\n') }
 
 const Item = {
-	of: (input, additional) => {
-		let data = `Item.of('${input}'${additional ? `, ${getString(additional)}` : ""})`
-		return {
-			item: true,
-			toString: () => data,
-			weakNBT: () => {
-				data += ".weakNBT()"
-				return { toString: () => data, item: true }
-			},
-			enchant: (enchant, level) => {
-				data += `.enchant('${enchant}', ${level})`
-				return { toString: () => data, item: true }
-			}
-		}
-	}
+    of: (input, additional) => {
+        let inp = input
+        let data = `Item.of('${input}'${additional ? `, ${getString(additional)}` : ""})`
+        return {
+            item: true,
+            toString: () => data,
+            id: () => input,
+            weakNBT: () => {
+                data += ".weakNBT()"
+                return { toString: () => data, item: true }
+            },
+            enchant: (enchant, level) => {
+                data += `.enchant('${enchant}', ${level})`
+                return { toString: () => data, item: true }
+            }
+        }
+    }
 }
 
 const Fluid = {
-	of: (input, additional) => {
-		let data = `Fluid.of('${input}'${additional ? `, ${getString(additional)}` : ""})`
-		return {
-			fluid: true,
-			toString: () => data,
-		}
-	}
+    of: (input, additional) => {
+        let data = `Fluid.of('${input}'${additional ? `, ${getString(additional)}` : ""})`
+        return {
+            fluid: true,
+            toString: () => data,
+        }
+    }
 }
 
 const industrialforegoing = {
-	laser_lens: {
-		white: 'industrialforegoing:laser_lens0',
-		orange: 'industrialforegoing:laser_lens1',
-		magenta: 'industrialforegoing:laser_lens2',
-		light_blue: 'industrialforegoing:laser_lens3',
-		yellow: 'industrialforegoing:laser_lens4',
-		lime: 'industrialforegoing:laser_lens5',
-		pink: 'industrialforegoing:laser_lens6',
-		gray: 'industrialforegoing:laser_lens7',
-		light_gray: 'industrialforegoing:laser_lens8',
-		cyan: 'industrialforegoing:laser_lens9',
-		purple: 'industrialforegoing:laser_lens10',
-		blue: 'industrialforegoing:laser_lens11',
-		brown: 'industrialforegoing:laser_lens12',
-		green: 'industrialforegoing:laser_lens13',
-		red: 'industrialforegoing:laser_lens14',
-		black: 'industrialforegoing:laser_lens15'
-	}
+    laser_lens: {
+        white: 'industrialforegoing:laser_lens0',
+        orange: 'industrialforegoing:laser_lens1',
+        magenta: 'industrialforegoing:laser_lens2',
+        light_blue: 'industrialforegoing:laser_lens3',
+        yellow: 'industrialforegoing:laser_lens4',
+        lime: 'industrialforegoing:laser_lens5',
+        pink: 'industrialforegoing:laser_lens6',
+        gray: 'industrialforegoing:laser_lens7',
+        light_gray: 'industrialforegoing:laser_lens8',
+        cyan: 'industrialforegoing:laser_lens9',
+        purple: 'industrialforegoing:laser_lens10',
+        blue: 'industrialforegoing:laser_lens11',
+        brown: 'industrialforegoing:laser_lens12',
+        green: 'industrialforegoing:laser_lens13',
+        red: 'industrialforegoing:laser_lens14',
+        black: 'industrialforegoing:laser_lens15'
+    }
 }
-
-
 
 // const powahTiers = ['starter', 'basic', 'hardened', 'blazing', 'niotic', 'spirited', 'nitro'];
 
@@ -132,113 +132,118 @@ const industrialforegoing = {
 
 /* // bloodmagic altar
 recipes.forEach((recipe) => {
-	console.log(`${recipe.id.includes('kubejs:') ? '' : 'e.remove({ id: "' + recipe.id + '" })\n'}e.recipes.bloodmagic.altar(${getString(recipe.output)}, ${getString(recipe.input)})
-	.upgradeLevel(${recipe.altarLevel})
-	.altarSyphon(${recipe.syphon})
-	.consumptionRate(${recipe.consumptionRate})
-	.drainRate(${recipe.drainRate})
-	.id('${recipe.id.replace(/.*:/g, "kubejs:")}')\n`)
+    console.log(`${recipe.id.includes('kubejs:') ? '' : 'e.remove({ id: "' + recipe.id + '" })\n'}e.recipes.bloodmagic.altar(${getString(recipe.output)}, ${getString(recipe.input)})
+    .upgradeLevel(${recipe.altarLevel})
+    .altarSyphon(${recipe.syphon})
+    .consumptionRate(${recipe.consumptionRate})
+    .drainRate(${recipe.drainRate})
+    .id('${recipe.id.replace(/.*:/g, "kubejs:")}')\n`)
 }) */
 
 /* // bloodmagic alchemytable
 recipes.forEach((recipe) => {
-	console.log(`${recipe.id.includes('kubejs:') ? '' : 'e.remove({ id: "' + recipe.id + '" })\n'}e.recipes.bloodmagic.alchemytable(Item.of(${getString(recipe.output)}, ${recipe.count}),
-	[${recipe.inputs.map(getString).join(", ")}])
-	.syphon(${recipe.syphon})
-	.ticks(${recipe.ticks})
-	.upgradeLevel(${recipe.orbLevel})
-	.id('${recipe.id.replace(/.*:/g, "kubejs:")}')\n`)
+    console.log(`${recipe.id.includes('kubejs:') ? '' : 'e.remove({ id: "' + recipe.id + '" })\n'}e.recipes.bloodmagic.alchemytable(Item.of(${getString(recipe.output)}, ${recipe.count}),
+    [${recipe.inputs.map(getString).join(", ")}])
+    .syphon(${recipe.syphon})
+    .ticks(${recipe.ticks})
+    .upgradeLevel(${recipe.orbLevel})
+    .id('${recipe.id.replace(/.*:/g, "kubejs:")}')\n`)
 }) */
 
-/* // shapeless
-recipes.forEach((recipe) => {
-	console.log(`${recipe.id.includes('kubejs:') ? '' : 'e.remove({ id: "' + recipe.id + '" })\n'}e.shapeless(${getString(recipe.output)},
+// shapeless
+recipes.forEach(recipe => {
+    recipe.id = recipe.id ? recipe.id :
+        typeof (recipe.output) == 'object' ?
+            recipe.output.id() : recipe.output
+
+    recipe.id = recipe.id.replace(/.*:/g, "kubejs:shapeless/")
+    console.log(`${recipe.id.includes('kubejs:') ? '' : 'e.remove({ id: "' + recipe.id + '" })\n'}e.shapeless(${getString(recipe.output)},
 	[${recipe.inputs.map(getString).join(", ")}]
-).id('${recipe.id.replace(/.*:/g, "kubejs:shapeless/")}')\n`)
-}) */
+).id('${recipe.id}')\n`)
+})
 
 /* // botania runic altar
 recipes.forEach((recipe) => {
-	console.log(`e.recipes.botania.runic_altar(${getString(`${(recipe.count) > 1 ? `${recipe.count}x ` : ""}` + recipe.output)},
-	[${recipe.inputs.map(getString).join(", ")}],
-	${recipe.mana}
+    console.log(`e.recipes.botania.runic_altar(${getString(`${(recipe.count) > 1 ? `${recipe.count}x ` : ""}` + recipe.output)},
+    [${recipe.inputs.map(getString).join(", ")}],
+    ${recipe.mana}
 ).id('${recipe.id}')\n`)
 }) */
 
 /* // ars enchanting apparatus
 recipes.forEach((recipe) => {
-	console.log(`e.recipes.ars_nouveau.enchanting_apparatus(
-	[${recipe.inputs.map(getString).join(", ")}],
-	${getString(recipe.reagent)},
-	${getString(recipe.output)}${recipe.sourceCost ? `, ${recipe.sourceCost}` : ""
-		}
+    console.log(`e.recipes.ars_nouveau.enchanting_apparatus(
+    [${recipe.inputs.map(getString).join(", ")}],
+    ${getString(recipe.reagent)},
+    ${getString(recipe.output)}${recipe.sourceCost ? `, ${recipe.sourceCost}` : ""
+        }
 ).id('${recipe.id}')\n`)
 }) */
 
 /* // botania petal apothecary
 recipes.forEach((recipe) => {
-	console.log(`e.recipes.botania.petal_apothecary(
-	${getString(recipe.output)},
-	[${recipe.inputs.map(getString).join(", ")}],
+    console.log(`e.recipes.botania.petal_apothecary(
+    ${getString(recipe.output)},
+    [${recipe.inputs.map(getString).join(", ")}],
 ).id('${recipe.id}')\n`)
 }) */
 
-// shaped
+/* // shaped
 recipes.forEach(recipe => {
-	let finalOut = getString(recipe.output)
-	let finalPattern = recipe.pattern.map(getString).join(",\n\t")
-	let finalKey = Object.entries(recipe.key).map(([key, value]) => `${key}: ${getString(value)}`).join(",\n\t")
-	let finalId = `'kubejs:${recipe.id.replace(/:/g, "_")}')`
-	console.log(`e.remove({ id: '${recipe.id}' })
+    let finalOut = getString(recipe.output)
+    let finalPattern = recipe.pattern.map(getString).join(",\n\t")
+    let finalKey = Object.entries(recipe.key).map(([key, value]) => `${key}: ${getString(value)}`).join(",\n\t")
+    let finalId = `'kubejs:${recipe.id.replace(/:/g, "_")}')`
+    console.log(`e.remove({ id: '${recipe.id}' })
 e.shaped(${finalOut}, [
-	${finalPattern}
+    ${finalPattern}
 ], {
-	${finalKey}
+    ${finalKey}
 }).id(${finalId}\n`
-	)
-})
+    )
+}) */
 
 /* // bloodmagic arc
 recipes.forEach((recipe) => {
-	console.log(`${recipe.id.includes('kubejs:') ? '' : 'e.remove({ id: "' + recipe.id + '" })\n'}e.recipes.bloodmagic.arc(${getString(recipe.output)}, ${getString(recipe.input)}, ${getString(recipe.tool)})
-	.consumeIngredient(${recipe.consume})${recipe.extraOutputs ? `\n	.addedoutput(${getString(recipe.extraOutputs)})` : ""}
-	.id('${recipe.id.replace(/.*:/g, "kubejs:")}')\n`)
+    console.log(`${recipe.id.includes('kubejs:') ? '' : 'e.remove({ id: "' + recipe.id + '" })\n'}e.recipes.bloodmagic.arc(${getString(recipe.output)}, ${getString(recipe.input)}, ${getString(recipe.tool)})
+    .consumeIngredient(${recipe.consume})${recipe.extraOutputs ? `\n	.addedoutput(${getString(recipe.extraOutputs)})` : ""}
+    .id('${recipe.id.replace(/.*:/g, "kubejs:")}')\n`)
 }) */
 
 /* // mechanical crafting
 recipes.forEach(recipe => {
-	let finalOut = getString(recipe.output)
-	let finalPattern = recipe.pattern.map(getString).join(",\n\t")
-	let finalKey = Object.entries(recipe.key).map(([key, value]) => `${key}: ${getString(value)}`).join(",\n\t")
-	let finalId = recipe.id
-	console.log(`${recipe.id.includes('kubejs:') ? '' : 'e.remove({ id: "' + recipe.id + '" })'}
+    let finalOut = getString(recipe.output)
+    let finalPattern = recipe.pattern.map(getString).join(",\n\t")
+    let finalKey = Object.entries(recipe.key).map(([key, value]) => `${key}: ${getString(value)}`).join(",\n\t")
+    let finalId = recipe.id
+    console.log(`${recipe.id.includes('kubejs:') ? '' : 'e.remove({ id: "' + recipe.id + '" })'}
 e.recipes.create.mechanical_crafting(${finalOut}, [
-	${finalPattern}
+    ${finalPattern}
 ], {
-	${finalKey}
+    ${finalKey}
 }).id(${finalId}`)
 }) */
 
 /* // sequenced assembly
 recipes.forEach(recipe => {
-	let sequence = []
+    let sequence = []
 
-	recipe.sequence.forEach((step) => {
-		if (step.type == 'deploying') {
-			sequence.push(`e.recipes.create.deploying(${getString(step.output)}, ${getString(step.input)})`)
-		} else if (step.type == 'cutting') {
-			sequence.push(
-				`e.recipes.create.cutting(${getString(step.output)}, ${getString(step.input)}).processingTime(${step.processingTime})`
-			)
-		} else if (step.type == 'filling') {
-			sequence.push(`e.recipes.create.filling(${getString(step.output)}, ${getString(step.input)})`)
-		} else if (step.type == 'pressing') {
-			sequence.push(`e.recipes.create.pressing(${getString(step.output)}, ${getString(step.input)})`)
-		}
-	})
-	console.log(`${shouldRemove(recipe)}e.recipes.create.sequenced_assembly([${recipe.outputs.map(getString).join(", ")}], ${getString(recipe.input)}, [
-		${sequence.join(",\n\t	")}
-	], ${getString(recipe.transitionalItem)}, ${recipe.loops})
-		.id('${recipe.id.replace(/.*:/g, "kubejs:")}')
-	`)
+    recipe.sequence.forEach((step) => {
+        if (step.type == 'deploying') {
+            sequence.push(`e.recipes.create.deploying(${getString(step.output)}, ${getString(step.input)})`)
+        } else if (step.type == 'cutting') {
+            sequence.push(
+                `e.recipes.create.cutting(${getString(step.output)}, ${getString(step.input)}).processingTime(${step.processingTime})`
+            )
+        } else if (step.type == 'filling') {
+            sequence.push(`e.recipes.create.filling(${getString(step.output)}, ${getString(step.input)})`)
+        } else if (step.type == 'pressing') {
+            sequence.push(`e.recipes.create.pressing(${getString(step.output)}, ${getString(step.input)})`)
+        }
+    })
+    console.log(`${shouldRemove(recipe)}e.recipes.create.sequenced_assembly([${recipe.outputs.map(getString).join(", ")}], ${getString(recipe.input)}, [
+        ${sequence.join(",\n\t	")}
+    ], ${getString(recipe.transitionalItem)}, ${recipe.loops})
+        .id('${recipe.id.replace(/.*:/g, "kubejs:")}')
+    `)
 }) */
