5308eb7076c8c431946f908384865b4c37e95055
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-08-23 20:36:35 -0500

    fix bloodmagic alchemy array textures

--- minecraft/kubejs/server_scripts/mods/bloodmagic/array.js
+++ minecraft/kubejs/server_scripts/mods/bloodmagic/array.js
@@ -1,22 +1,22 @@
 ServerEvents.recipes(e => {
     e.recipes.bloodmagic.array('powah:charged_snowball', '#forge:rods/copper', '#forge:gems/fluorite')
-        .texture('bindinglightningarray')
+        .texture('bloodmagic:textures/models/alchemyarrays/bindinglightningarray.png')
         .id(`kubejs:array/charged_snowball`)
 
     e.recipes.bloodmagic.array('minecraft:heart_of_the_sea', 'architects_palette:algal_lamp', '#forge:gems/arcanum')
-        .texture('watersigil')
+        .texture('bloodmagic:textures/models/alchemyarrays/watersigil.png')
         .id(`kubejs:array/heart_of_the_sea`)
 
     e.recipes.bloodmagic.array('bloodmagic:arcaneashes', 'architects_palette:moonstone', '#forge:ingots/silver')
-        .texture('moonarray')
+        .texture('bloodmagic:textures/models/alchemyarrays/moonarray.png')
         .id(`kubejs:array/night`)
 
     e.recipes.bloodmagic.array('bloodmagic:arcaneashes', 'architects_palette:sunstone', '#forge:ingots/sunmetal')
-        .texture('sunarray')
+        .texture('bloodmagic:textures/models/alchemyarrays/sunarray.png')
         .id(`kubejs:array/day`)
 
     e.remove({ id: 'bloodmagic:array/divinationsigil' })
     e.recipes.bloodmagic.array('bloodmagic:divinationsigil', 'ars_nouveau:ritual_scrying', 'bloodmagic:blankslate')
-        .texture('divinationsigil')
+        .texture('bloodmagic:textures/models/alchemyarrays/divinationsigil.png')
         .id(`kubejs:array/divinationsigil`)
 })
