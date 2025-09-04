0a5416b714ecaaf1335aacfbeb07e981cc43369b
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-09-03 20:05:53 -0500

    missed a tag file oops

---
+++ minecraft/kubejs/server_scripts/mods/quark/tags.js
@@ -0,0 +1,9 @@
+ServerEvents.tags('item', e => {
+    // quark color runes tag
+    e.add('quark:runes', ['kubejs:white_rune', 'kubejs:light_gray_rune', 'kubejs:gray_rune',
+        'kubejs:black_rune', 'kubejs:brown_rune', 'kubejs:red_rune', 'kubejs:orange_rune',
+        'kubejs:yellow_rune', 'kubejs:lime_rune', 'kubejs:green_rune', 'kubejs:cyan_rune',
+        'kubejs:light_blue_rune', 'kubejs:blue_rune', 'kubejs:purple_rune', 'kubejs:magenta_rune',
+        'kubejs:pink_rune',
+    ])
+})
