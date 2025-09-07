c9ab2fd9ef588567982ae5a5c56d44c9cb5132a8
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-09-07 05:22:26 -0500

    fix broken pearl crushing

--- minecraft/kubejs/server_scripts/mods/kubejs/crushing.js
+++ minecraft/kubejs/server_scripts/mods/kubejs/crushing.js
@@ -1,3 +1,3 @@
 ServerEvents.recipes(e => {
-    allCrushing(e, 'thermal:ender_pearl_dust', 'minecraft:ender_pearl', 'kubejs:crushing', true)
+    allCrushing(e, 'thermal:ender_pearl_dust', 'minecraft:ender_pearl', 'kubejs:crushing', 1, 1, true)
 })
