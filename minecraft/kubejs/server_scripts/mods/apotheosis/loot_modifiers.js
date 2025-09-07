ccee90b476e1f5716b864b746b8a7b305d32f49e
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-09-06 22:23:34 -0500

    remove apoth scrap loot for now

--- minecraft/kubejs/server_scripts/mods/apotheosis/loot_modifiers.js
+++ minecraft/kubejs/server_scripts/mods/apotheosis/loot_modifiers.js
@@ -1,28 +1,30 @@
-LootJS.modifiers(event => {
-    let valuables = [
-        ["minecraft:iron_ingot", 15],
-        ["minecraft:coal", 5],
-        ["minecraft:gold_ingot", 20],
-        ["minecraft:diamond", 30],
-        ["minecraft:ancient_debris", 45],
-        ["minecraft:netherite_scrap", 40],
-        ["minecraft:netherite_ingot", 100],
-        ["minecraft:golden_apple", 20],
-        ["minecraft:enchanted_golden_apple", 40]
-    ]
+LootJS.modifiers(e => {
+    // let valuables = [
+    //     ["minecraft:iron_ingot", 15],
+    //     ["minecraft:coal", 5],
+    //     ["minecraft:gold_ingot", 20],
+    //     ["minecraft:diamond", 30],
+    //     ["minecraft:ancient_debris", 45],
+    //     ["minecraft:netherite_scrap", 40],
+    //     ["minecraft:netherite_ingot", 100],
+    //     ["minecraft:golden_apple", 20],
+    //     ["minecraft:enchanted_golden_apple", 40]
+    // ]
 
-    function addScrap(scrap, baseChance) {
-        valuables.forEach(v => {
-            event
-                .addLootTypeModifier(LootType.CHEST)
-                .matchLoot(v[0])
-                .randomChance(v[1]/baseChance)
-                .addLoot(scrap)
-        })
-    }
-    addScrap('apotheosis:mythic_material', 1250)
-    addScrap('apotheosis:epic_material', 800)
-    addScrap('apotheosis:rare_material', 400)
-    addScrap('apotheosis:uncommon_material', 200)
-    addScrap('apotheosis:common_material', 100)
+
+    // function addScrap(scrap, baseChance) {
+    //     valuables.forEach(valuableItem => {
+    //         event
+    //             .addLootTypeModifier(LootType.CHEST)
+    //             .matchLoot(valuableItem[0])
+    //             .randomChance(valuableItem[1]/baseChance)
+    //             .addLoot(scrap)
+    //     })
+    // }
+    // addScrap('apotheosis:mythic_material', 1250)
+    // addScrap('apotheosis:epic_material', 800)
+    // addScrap('apotheosis:rare_material', 400)
+    // addScrap('apotheosis:uncommon_material', 200)
+    // addScrap('apotheosis:common_material', 100)
 })
+
