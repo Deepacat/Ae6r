fc69bb5bde6284dfa6bd702b45bcdca9ef44ad31
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-09-08 05:15:03 -0500

    aspectus shard aether loot table testing

--- minecraft/kubejs/server_scripts/mods/aether/loot_tables.js
+++ minecraft/kubejs/server_scripts/mods/aether/loot_tables.js
@@ -1,3 +1,17 @@
 LootJS.modifiers(e => {
-    
+    let aspectusShards = ['aer', 'aqua', 'ignis', 'terra', 'ordo', 'perditio', 'mortuus', 'cognitio', 'praecantatio']
+
+    // 
+    let dungeonRewardShards = aspectusShards.map(
+        (aspect) => Item.of(`kubejs:${aspect}_aspectus_shard_block`).withChance(0.0005)
+    )
+
+    // Bronze dungeon boss chest
+    e.addLootTableModifier("aether:chests/dungeon/bronze/bronze_dungeon_reward")
+        .addWeightedLoot(
+            [0, 2],
+            dungeonRewardShards
+        )
+        // always give 1 shard block
+        .addLoot(Item.of('kubejs:aspectus_shard_block'))
 })
