fb3ee593eaa1ecd69b03e725c5efdf0b570b521e
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-09-10 04:44:11 -0500

    restrictions improvements

--- minecraft/kubejs/client_scripts/restrictions.js
+++ minecraft/kubejs/client_scripts/restrictions.js
@@ -1,44 +1,78 @@
-// // file for restricting blocks from use until conditions are met
-// // client part. (server, client, startup)
-// // cancels use/place client side
+///@ts-check
 
-// let restrictions = global.restrictions
+// file for restricting blocks from use until conditions are met
+// server/client part. (server, client)
+// cancels use/place and alerts player
+// copied exact file between client and server
 
-// /**
-//  * @param {Internal.ItemClickedEventJS | Internal.BlockRightClickedEventJS | Internal.BlockPlacedEventJS} event
-//  * @param {string | number} blockOrItem
-//  */
-// function canUse(event, blockOrItem) {
-//     const { player } = event
-//     const level = player.level
+// @ts-ignore
+let restrictions = global.restrictions
 
-//     if (!restrictions[blockOrItem]) { return true }
+/**
+ * @param {Internal.ItemClickedEventJS | Internal.BlockRightClickedEventJS | Internal.BlockPlacedEventJS} event
+ * @param {string | number} blockOrItem
+ */
+function canUse(event, blockOrItem) {
+    const { player } = event
+    const level = player.level
 
-//     const errors = []
-//     for (const condition of restrictions[blockOrItem].conditions) {
-//         if (condition.dimension && condition.dimension != level.dimension) {
-//             errors.push(`§cYou can't use ${blockOrItem} in this dimension, it can only be used in: ${condition.dimension}`)
-//         }
-//         if (condition.stage && !player.stages.has(condition.stage)) {
-//             errors.push(`§cYou can't use ${blockOrItem} yet, you must complete gamestage: ${condition.stage}`)
-//         }
-//     }
+    let cd
+    // only do persistent data on serverside
+    if (!level.isClientSide()) {
+        // @ts-ignore
+        cd = player.persistentData.restrictionTellCd
 
-//     if (errors.length > 0) {
-//         return false
-//     }
-//     return true
-// }
+        if (!cd) { cd = {} }
+    }
 
-// ItemEvents.rightClicked(event => {
-//     if (!canUse(event, event.item.id)) { event.cancel() }
-// })
 
-// BlockEvents.rightClicked(event => {
-//     if (!canUse(event, event.block.id)) { event.cancel() }
-//     if (!canUse(event, event.item.id)) { event.cancel() }
-// })
+    if (!restrictions[blockOrItem]) { return true }
 
-// BlockEvents.placed(event => {
-//     if (!canUse(event, event.block.id)) { event.cancel() }
-// })
+    const errors = []
+    for (const condition of restrictions[blockOrItem].conditions) {
+        if (condition.sage && !player.stages.has(condition.sage)) {
+            errors.push(`§cYou can't use §b${blockOrItem} §cyet, you must complete §agamestage "${condition.sage}"`)
+        }
+        if (condition.dimension) {
+            if (condition.dimension != level.dimension) {
+                let dimName = condition.dimension.split(':')[1]
+                if (condition.dimStage && !player.stages.has(condition.dimStage)) {
+                    errors.push(`§cYou can't use §b"${blockOrItem}" §coutside of §b"${dimName}" §cyet, you must complete §agamestage "${condition.dimStage}"`)
+                } else {
+                    errors.push(`§cYou can't use §b"${blockOrItem}" §cin this dimension, it can only be used in §a"${dimName}"`)
+                }
+            }
+        }
+    }
+
+    if (errors.length > 0) {
+        // only run tells and cd persistent data on server
+        if (!level.isClientSide()) {
+            if (cd[blockOrItem] == 0 || !cd[blockOrItem]) {
+                cd[blockOrItem] = 1
+                event.server.scheduleInTicks(100, () => { cd[blockOrItem] = 0 })
+                for (const error of errors) {
+                    // @ts-ignore
+                    player.tell(error)
+                }
+                // @ts-ignore
+                player.tell('\n')
+            }
+        }
+        return false
+    }
+    return true
+}
+
+ItemEvents.rightClicked(event => {
+    if (!canUse(event, event.item.id)) { event.cancel() }
+})
+
+BlockEvents.rightClicked(event => {
+    if (!canUse(event, event.block.id)) { event.cancel() }
+    if (!canUse(event, event.item.id)) { event.cancel() }
+})
+
+BlockEvents.placed(event => {
+    if (!canUse(event, event.block.id)) { event.cancel() }
+})
--- minecraft/kubejs/server_scripts/restrictions.js
+++ minecraft/kubejs/server_scripts/restrictions.js
@@ -1,8 +1,9 @@
 ///@ts-check
 
 // file for restricting blocks from use until conditions are met
-// client part. (server, client)
-// cancels use/place server side and alerts player
+// server/client part. (server, client)
+// cancels use/place and alerts player
+// copied exact file between client and server
 
 // @ts-ignore
 let restrictions = global.restrictions
@@ -15,10 +16,15 @@ function canUse(event, blockOrItem) {
     const { player } = event
     const level = player.level
 
-    // @ts-ignore
-    let cd = player.persistentData.restrictionTellCd
+    let cd
+    // only do persistent data on serverside
+    if (!level.isClientSide()) {
+        // @ts-ignore
+        cd = player.persistentData.restrictionTellCd
 
-    if (!cd) { cd = {} }
+        if (!cd) { cd = {} }
+    }
+
 
     if (!restrictions[blockOrItem]) { return true }
 
@@ -31,30 +37,27 @@ function canUse(event, blockOrItem) {
             if (condition.dimension != level.dimension) {
                 let dimName = condition.dimension.split(':')[1]
                 if (condition.dimStage && !player.stages.has(condition.dimStage)) {
-                    errors.push(`§cYou can't use §b${blockOrItem} §coutside of §b"${dimName}" §cyet, you must complete §agamestage "${condition.dimStage}"`)
+                    errors.push(`§cYou can't use §b"${blockOrItem}" §coutside of §b"${dimName}" §cyet, you must complete §agamestage "${condition.dimStage}"`)
                 } else {
-                    errors.push(`§cYou can't use §b${blockOrItem} §cin this dimension, it can only be used in §a${dimName   }`)
+                    errors.push(`§cYou can't use §b"${blockOrItem}" §cin this dimension, it can only be used in §a"${dimName}"`)
                 }
             }
         }
-        // if (condition.stage && !player.stages.has(condition.stage)) {
-        //     errors.push(`§cYou can't use §b${blockOrItem} §cyet, you must complete §agamestage "${condition.stage}"`)
-        // }
-        // if (condition.dimension && condition.dimension != level.dimension) {
-        //     errors.push(`§cYou can't use §b${blockOrItem} §cin this dimension, it can only be used in §a${condition.dimension}`)
-        // }
     }
 
     if (errors.length > 0) {
-        if (cd[blockOrItem] == 0 || !cd[blockOrItem]) {
-            cd[blockOrItem] = 1
-            event.server.scheduleInTicks(100, () => { cd[blockOrItem] = 0 })
-            for (const error of errors) {
+        // only run tells and cd persistent data on server
+        if (!level.isClientSide()) {
+            if (cd[blockOrItem] == 0 || !cd[blockOrItem]) {
+                cd[blockOrItem] = 1
+                event.server.scheduleInTicks(100, () => { cd[blockOrItem] = 0 })
+                for (const error of errors) {
+                    // @ts-ignore
+                    player.tell(error)
+                }
                 // @ts-ignore
-                player.tell(error)
+                player.tell('\n')
             }
-            // @ts-ignore
-            player.tell('\n')
         }
         return false
     }
