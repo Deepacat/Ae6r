316913f04bcf1bd075812b8e05599c15e78aaed8
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-09-29 03:24:18 -0500

    wizreborn recipes

--- minecraft/kubejs/server_scripts/mods/wizards_reborn/shaped.js
+++ minecraft/kubejs/server_scripts/mods/wizards_reborn/shaped.js
@@ -6,7 +6,7 @@ ServerEvents.recipes(e => {
         'D  '
     ], {
         A: '#forge:ingots/arcane_gold',
-        B: '#kubejs:aspectus_shards',
+        B: ['minecraft:conduit', 'kubejs:cognito_aspectus_shard'],
         C: 'wizards_reborn:arcane_wood_branch',
         D: '#forge:storage_blocks/arcane_gold'
     }).id('kubejs:wizards_reborn/shaped/arcane_wand')
@@ -19,7 +19,20 @@ ServerEvents.recipes(e => {
     ], {
         A: 'irons_spellbooks:upgrade_orb',
         B: '#forge:storage_blocks/arcanum',
-        C: 'wizards_reborn:arcane_wood_branch',
-        D: '#forge:rods/silver'
+        C: 'wizards_reborn:arcane_wand',
+        D: 'kubejs:treated_arcane_handle'
     }).id('kubejs:wizards_reborn/shaped/wissen_wand')
+
+    e.remove({ output: 'wizards_reborn:arcane_workbench' })
+    e.shaped('wizards_reborn:arcane_workbench', [
+        'AAA',
+        'BCB',
+        'DED',
+    ], {
+        A: 'thermal:red_rockwool',
+        B: 'irons_spellbooks:arcane_salvage',
+        C: '#forge:storage_blocks/arcane_gold',
+        D: 'naturesaura:ancient_slab',
+        E: 'naturesaura:ancient_planks'
+    }).id('kubejs:wizards_reborn/shaped/arcane_workbench')
 })
