d0b6912c784216d72dcc00da44b316a163188b3a
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-10-02 03:19:52 -0500

    Add ritual for adding wissen to arcane workbench, improve ritual code

--- minecraft/kubejs/server_scripts/mods/summoningrituals/altar.js
+++ minecraft/kubejs/server_scripts/mods/summoningrituals/altar.js
@@ -1,27 +1,64 @@
+function commentItem(lore) {
+    return SummoningOutput.item(
+        Item.of('ftbquests:missing_item', {
+            display: {
+                Name: "\"Comment\"",
+                Lore: lore
+            },
+            Enchantments: [{}],
+            "quark:RuneColor": "red"
+        })
+    ).offset(0, -999, 0)
+}
+
+function findingHelperItem(itemId) {
+    return SummoningOutput.item(
+        Item.of(itemId, {
+            display: {
+                Name: "\"Finding Helper Item\"",
+                Lore: [
+                    "\"This output item is not given, it has been placed for your ease of finding this summoning recipe!\""
+                ]
+            },
+            Enchantments: [{}],
+            "quark:RuneColor": "red"
+        })
+    ).offset(0, -999, 0)
+}
+
 // flutter summoning ritual
 ServerEvents.recipes(e => {
+    // summon 2 flutters
     e.recipes.summoningrituals.altar('#summoningrituals:flutter_catalysts')
         .input('2x minecraft:flowering_azalea')
         .input('2x minecraft:spore_blossom')
-        .recipeTime(200)
         .mobOutput(SummoningOutput.mob('alexsmobs:flutter')
             .count(2)
             .offset(0, 1, 0)
             .data({ ForgeData: { time_sheared: 12000 } })
         )
-        .itemOutput(
-            SummoningOutput.item(
-                Item.of('kubejs:lush_silk', 69, {
-                    display: {
-                        Name: "\"Suspiciously non existent lush silk\"",
-                        Lore: [
-                            "\"This has been placed for your ease of finding this summoning recipe!\"",
-                            "\"(item spawns at y -999)\"",
-                            "\"This ritual only works in lush caves!\""
-                        ]
-                    }
-                })
-            ).offset(0, -999, 0)
-        )
+        .recipeTime(20 * 10)
+        .itemOutput(findingHelperItem('kubejs:lush_silk'))
+        .itemOutput(commentItem(["\"This ritual only works in lush caves!\""]))
         .id('kubejs:summoningrituals/altar/flutter')
+
+    // allay to wissen
+    e.recipes.summoningrituals.altar('#forge:storage_blocks/arcanum')
+        .input('3x minecraft:amethyst_shard')
+        .input('3x minecraft:experience_bottle')
+        .sacrifice('minecraft:allay')
+        .sacrificeRegion(9, 9)
+        .recipeTime(20 * 10)
+        .itemOutput(findingHelperItem('wizards_reborn:arcanum'))
+        .itemOutput(commentItem(["\"This ritual outputs 100 wissen to an arcane workbench below the altar.\""]))
+        .id('kubejs:summoningrituals/altar/wissen_allay')
+
+    // quenched allay to wissen
+    e.recipes.summoningrituals.altar('hexcasting:quenched_allay_shard')
+        .input(Ingredient.of('#forge:gems/arcanum', 3))
+        .recipeTime(20 * 3)
+        .itemOutput(findingHelperItem('wizards_reborn:arcanum'))
+        .itemOutput(commentItem(["\"This ritual outputs 1000 wissen to an arcane workbench below the altar.\""]))
+        .id('kubejs:summoningrituals/altar/wissen_quenched')
 })
+
---
+++ minecraft/kubejs/server_scripts/mods/summoningrituals/custom_ritual_interactions.js
@@ -0,0 +1,61 @@
+// prevent flutter ritual outside lush caves
+SummoningRituals.start(e => {
+    if (!(e.recipe.id == 'kubejs:summoningrituals/altar/flutter')) { return }
+
+    let biome = e.level.getBiome(e.pos).unwrapKey().get().location()
+    if (biome != 'minecraft:lush_caves') {
+        if (e.player) {
+            e.player.setStatusMessage('You must be in the lush caves to perform this ritual.')
+        }
+        e.cancel()
+    }
+})
+
+SummoningRituals.start(e => {
+    if (!e.recipe.id == 'kubejs:summoningrituals/altar/wissen') { return }
+
+    if (e.level.getBlock(e.pos.below()) != 'wizards_reborn:arcane_workbench') {
+        e.player.tell('You can only perform this ritual with the altar above an arcane workbench.')
+        e.cancel()
+    }
+})
+
+SummoningRituals.complete(e => {
+    let wissenCount
+    switch (e.recipe.id) {
+        case 'kubejs:summoningrituals/altar/wissen_allay':
+            wissenCount = 100;
+            break
+        case 'kubejs:summoningrituals/altar/wissen_quenched':
+            wissenCount = 1000;
+            break
+        default:
+            break
+    }
+
+    if (!(wissenCount > 0)) return
+
+    let blockBelow = e.level.getBlock(e.pos.below())
+    if (blockBelow != 'wizards_reborn:arcane_workbench') {
+        e.cancel()
+        return
+    }
+
+    let wissenInTable = blockBelow.entityData.wissen
+
+    if (wissenInTable > (10000 - wissenCount)) {
+        e.cancel()
+        return
+    }
+
+    blockBelow.mergeEntityData({ wissen: wissenInTable + wissenCount })
+
+    // send a block update after merging data so it updates clientside
+    e.level.markAndNotifyBlock(
+        blockBelow.pos,
+        e.level.getChunkAt(blockBelow.pos),
+        blockBelow.blockState,
+        blockBelow.blockState,
+        3, 512
+    )
+})
