bf258dfe84da89c0401c2ffaae854fde63a362a9
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-10-13 01:42:05 -0500

    sdrp cleanup

--- minecraft/kubejs/client_scripts/rpcClient.js
+++ minecraft/kubejs/client_scripts/rpcClient.js
@@ -3,41 +3,10 @@
 // Client part (Server, Client)
 
 NetworkEvents.dataReceived('ae6r:rpc', e => {
+    console.log(e.data)
     SDRP.setState(
-        `Tech: Ch${e.data.tech}, Magic: Ch${e.data.magic}, ` +
-        `Playtime: ${(e.player.stats.playTime/60/60/20).toFixed(2)}h`,
+        `Tech: ${e.data.tech}, Magic: ${e.data.magic}, 
+        Playtime: ${(e.player.stats.playTime / 60 / 60 / 20).toFixed(2)}h`,
         "Quest Progress", "quest"
     )
-    // SDRP.setState(
-    //     `STATUSTEST1 STATUSTEST2 STATUSTEST3 STATUSTEST4 STATUSTEST5 STATUSTEST6`
-    //     , "test", "test"
-    // )
 })
-
-
-if (global.fsdifjsdjfdi) {
-    global.tier = 0; // this is clientside tier anyways
-    ClientEvents.loggedIn(() => {
-        SDRP.setState("sdrp.ulv", "ULV", "ulv")
-    })
-
-    ClientEvents.loggedOut(() => {
-        global.tier = 0;
-    })
-
-    NetworkEvents.dataReceived("moni:rpc", (e) => {
-        global.tier = Number(e.data.tier ?? TIER_ULV);
-        let tierID = TIER_ID_MAPPING[Math.round(global.tier)]; // putting val inside the global var turns it into float
-        SDRP.setState(`sdrp.${tierID.toLowerCase()}`, tierID, tierID.toLowerCase())
-        // console.log("SDRP Update Client By Network")
-        // console.log(`New tier: ${global.tier} ${tierID}`)
-    })
-
-    // this event is slightly delayed when running an integrated server
-    sdrp.dimension_change(ev => {
-        let tierID = TIER_ID_MAPPING[Math.round(global.tier)];
-        ev.updateSDRPState(`sdrp.${tierID.toLowerCase()}`, tierID, tierID.toLowerCase())
-        // console.log("SDRP Update Client By Dim")
-    })
-}
-
--- minecraft/kubejs/server_scripts/misc scripts/rpcServer.js
+++ minecraft/kubejs/server_scripts/misc scripts/rpcServer.js
@@ -2,13 +2,6 @@
 // Thanks to Monifactory for reference
 // Server part (Server, Client)
 
-// let getQuestData = (player) => FTBQuests.getData(player)
-// let getQuestObject = (level, id) => FTBQuests.getObject(level, id)
-// let isQuestComplete = (player, questObject) => getQuestData(player).isCompleted(questObject)
-// let getQuestProgress = (player, questObject) => getQuestData(player).getProgress(questObject)
-// let setQuestProgress = (player, questObject, progress) => getQuestData(player).setProgress(questObject, progress)
-// let addQuestProgress = (player, questObject, amount) => getQuestData(player).addProgress(questObject, amount)
-
 /** @private */
 const questProgression = {
     magic: [
@@ -33,82 +26,27 @@ function updateRPC(e) {
 
     for (let [path, chapter] of Object.entries(questProgression)) {
         for (let questsInChapter of chapter) {
-            // console.log(`Checking chapter ${chapter.indexOf(questsInChapter) + 1} of ${path}`)
             for (let questId of questsInChapter) {
                 let questObj = FTBQuests.getObject(e.player.level, questId)
                 let isCompleted = FTBQuests.getData(e.player).isCompleted(questObj)
-                // console.log(`checking if ${questObj.getTitle()} is completed`)
                 if (isCompleted) {
-                    // console.log(`${questObj.getTitle()} is completed`)
-                    // console.log(`Setting ${path} chapter to ${chapter.indexOf(questsInChapter) + 1}`)
                     progress[path] = chapter.indexOf(questsInChapter) + 1
                 }
             }
         }
     }
-    console.log(`Player ${e.player.name} logged in with magic chapter ${progress.magic} and tech chapter ${progress.tech}`)
     e.player.sendData('ae6r:rpc', progress)
 }
-
-PlayerEvents.inventoryOpened(e => {
-    updateRPC(e)
-})
 
 PlayerEvents.loggedIn(e => {
     updateRPC(e)
 })
 
-if (global.fsdifjsdjfdi) {
-    /** @private */
-    const ___TIER_QUEST_MAPPING = [
-        // all of these are on the progression page
-        "DEFAULT",
-        "61CC4AD6FAE15248", // lv
-        "6C028CD89745098E", // mv
-        "48CAEBEE83D7C791", // hv
-        "30AAFB97813DEB6C", // ev
-        "6E27637A9C94A0AA", // iv
-        "646B8CD10787D899", // luv
-        "11B25506F679ACBD", // zpm
-        "4B497D256FB3983F", // uv
-        "0A34290F579BB4E2", // uhv
-        "63812416270292A4", // uev
-        "60D863C48FBFF5A3", // uiv
-        "1A6F087E10DC036E", // max
-        "63280C5F37A22345" // creative chest (end)
-    ]
+PlayerEvents.tick(e => {
+    if (Utils.server.tickCount % (20 * 60) != 0) { return }
+    updateRPC(e)
+})
 
-    FTBQuestsEvents.completed(event => {
-        const id = ___TIER_QUEST_MAPPING.findIndex(v => (v === event.object.codeString))
-        if (id === -1) return;
-        ___updateTierRPC(id, event.player);
-    })
-
-    PlayerEvents.loggedIn(event => {
-        const quests = FTBQuests.getServerDataFromPlayer(event.player);
-
-        let highestLevel = 0;
-        for (const key in ___TIER_QUEST_MAPPING) {
-            let value = ___TIER_QUEST_MAPPING[key];
-            if (value === "DEFAULT") continue;
-            if (quests.isCompleted(value))
-                highestLevel = key;
-        }
-        ___updateTierRPC(highestLevel, event.player)
-    })
-
-    /**
-     * @private
-     * @param {number} tier
-     * @param {Internal.Player} player
-     */
-    const ___updateTierRPC = (tier, player) => {
-        if (!player) {
-            console.log("!! READ THIS SERVER.LOG DUMMY !!")
-            console.log("!! DONT RIGHT CLICK -> 'COMPLETE INSTANTLY', IT WONT SEND THE PLAYER !!")
-            return;
-        }
-        player.sendData("moni:rpc", { tier: tier });
-        // console.log("SDRP Sent Update to client");
-    }
-}
+FTBQuestsEvents.completed(e => {
+    updateRPC(e)
+})
