29dcc3f8a86dc40f08d94fe89c7e939758e381f4
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-10-26 02:35:37 -0500

    add type params to schema functions

--- minecraft/kubejs/startup_scripts/schemas/farmers_delight.js
+++ minecraft/kubejs/startup_scripts/schemas/farmers_delight.js
@@ -1,4 +1,9 @@
 // priority: 0
+
+/**
+ * @param {Internal.RecipeSchemaRegistryEventJS} e
+ * @param {{ components?: Special.RecipeComponentMap; anyFloatNumber?: Internal.NumberComponent$FloatRange; anyString?: Internal.StringComponent; bool?: Internal.BooleanComponent; id: any; intNumber: any; filteredString?: (...args: any[]) => Internal.RecipeComponent<any>; nonBlankString?: Internal.StringComponent; floatNumber?: Internal.NumberComponent$FloatRange; inputItem: any; outputItem: any; inputFluid?: Internal.FluidComponents$1; outputFluid?: Internal.FluidComponents$2; outputFluidOrItem?: Internal.OrRecipeComponent<any, any>; fluidTag?: Internal.RecipeComponent<any>; blockTag?: Internal.TagKeyComponent<any>; inputStackedItem?: any; inputFluidOrFluidTag?: (tagKeyStr: any) => Internal.OrRecipeComponent<Internal.InputFluid, any>; outputFluidOrFluidTag?: (tagKeyStr: any) => Internal.OrRecipeComponent<Internal.OutputFluid, any>; inputFluidOrItem?: (tagKeyStr: any) => Internal.OrRecipeComponent<InputItem, any>; inputFluidOrStackedItem?: (tagKeyStr: any) => any; heatCondition?: Internal.RecipeComponent<any>; }} c
+ */
 function recipeSchema_farmers_delight(e, c) {
     if (Platform.isLoaded('farmersdelight')) {
         let itemOrAction = c.inputItem.or(
--- minecraft/kubejs/startup_scripts/schemas/immersiveengineering.js
+++ minecraft/kubejs/startup_scripts/schemas/immersiveengineering.js
@@ -1,6 +1,10 @@
 // priority: 0
 // https://github.com/BluSunrize/ImmersiveEngineering/tree/1.20.1/src/main/java/blusunrize/immersiveengineering/common/crafting/serializers
 
+/**
+ * @param {Internal.RecipeSchemaRegistryEventJS} e
+ * @param {{ components?: Special.RecipeComponentMap; anyFloatNumber?: Internal.NumberComponent$FloatRange; anyString?: Internal.StringComponent; bool?: Internal.BooleanComponent; id: any; intNumber: any; filteredString?: (...args: any[]) => Internal.RecipeComponent<any>; nonBlankString?: Internal.StringComponent; floatNumber?: Internal.NumberComponent$FloatRange; inputItem: any; outputItem: any; inputFluid?: Internal.FluidComponents$1; outputFluid?: Internal.FluidComponents$2; outputFluidOrItem?: Internal.OrRecipeComponent<any, any>; fluidTag?: Internal.RecipeComponent<any>; blockTag?: Internal.TagKeyComponent<any>; inputStackedItem?: any; inputFluidOrFluidTag?: (tagKeyStr: any) => Internal.OrRecipeComponent<Internal.InputFluid, any>; outputFluidOrFluidTag?: (tagKeyStr: any) => Internal.OrRecipeComponent<Internal.OutputFluid, any>; inputFluidOrItem?: (tagKeyStr: any) => Internal.OrRecipeComponent<InputItem, any>; inputFluidOrStackedItem?: (tagKeyStr: any) => any; heatCondition?: Internal.RecipeComponent<any>; }} c
+ */
 function recipeSchema_immersiveengineering(e, c) {
     if (Platform.isLoaded('immersiveengineering')) {
         // ie uses a special custom ingredient format for stacked inputs
